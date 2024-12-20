28be23163a7ef825da6353b49a847ccec486f78b
Luc Pham <phamtienluc0601@gmail.com>
2024-12-20 21:32:23 +0700

    Add exception handling and some configurations for microservices

---
+++ auth-service/src/main/java/ChitChat/auth_service/exception/AppException.java
@@ -0,0 +1,14 @@
+package ChitChat.auth_service.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class AppException extends RuntimeException{
+    private ErrorCode errorCode;
+}
---
+++ auth-service/src/main/java/ChitChat/auth_service/exception/ErrorCode.java
@@ -0,0 +1,32 @@
+package ChitChat.auth_service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public enum ErrorCode {
+
+    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
+
+    INVALID_KEY(1001, "Uncategorized error", HttpStatus.BAD_REQUEST),
+
+    ENTITY_EXISTED(1002, "Entity existed!", HttpStatus.BAD_REQUEST),
+
+    ENTITY_NOT_EXISTED(1005, "Entity not existed", HttpStatus.NOT_FOUND),
+
+    NO_REFRESH_TOKEN(1010, "You don't have refresh token in cookies", HttpStatus.BAD_REQUEST),
+
+    INVALID_ACCESS_TOKEN(1011, "Your access token is not valid", HttpStatus.BAD_REQUEST), 
+
+    ;
+
+    private int code;
+    private String message;
+    private HttpStatusCode httpStatusCode;
+}
