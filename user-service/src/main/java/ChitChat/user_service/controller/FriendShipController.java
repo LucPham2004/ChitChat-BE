b3a1bc55344c0f0a5a33107260e5d7afc8ef1911
Luc Pham <phamtienluc0601@gmail.com>
2024-12-20 20:29:27 +0700

    Add User Controller and Friendship Controller

---
+++ user-service/src/main/java/ChitChat/user_service/controller/FriendShipController.java
@@ -0,0 +1,84 @@
+package ChitChat.user_service.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import ChitChat.user_service.dto.response.ApiResponse;
+import ChitChat.user_service.dto.response.FriendShipResponse;
+import ChitChat.user_service.enums.FriendshipStatus;
+import ChitChat.user_service.mapper.FriendshipMapper;
+import ChitChat.user_service.service.FriendshipService;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@RestController
+@RequestMapping("/api/friendships")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class FriendshipController {
+
+    FriendshipService friendShipService;
+    FriendshipMapper mapper;
+
+    @GetMapping("/get/status")
+    public ApiResponse<FriendShipResponse> getFriendStatus(
+					@RequestParam Long senderId, 
+					@RequestParam Long recipientId) {
+            var response = this.friendShipService.getFriendStatus(senderId, recipientId);
+            if(response == null) {
+                return ApiResponse.<FriendShipResponse>builder()
+                            .code(1000)
+                            .message("2 Users are not Friends")
+                            .build();
+            }
+            return ApiResponse.<FriendShipResponse>builder()
+                            .code(1000)
+                            .message("Get friendship status from user with ID: " + senderId + 
+                                        " and user with ID: " + recipientId + " successfully!")
+                            .result(mapper.toFriendShipResponse(response))
+                            .build();
+    }
+
+    @PostMapping("/request")
+    public ApiResponse<FriendShipResponse> sendFriendRequest(
+					@RequestParam Long senderId, 
+					@RequestParam Long recipientId) {
+            var response = this.friendShipService.sendFriendRequest(senderId, recipientId);
+            return ApiResponse.<FriendShipResponse>builder()
+                            .code(1000)
+                            .message("Send friend request from user with ID: " + senderId + 
+                                        " to user with ID: " + recipientId + " successfully!")
+                            .result(mapper.toFriendShipResponse(response))
+                            .build();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ApiResponse<Void> deleteFriendShip(@PathVariable Long id) { 
+            this.friendShipService.deleteFriendShip(id);
+            return ApiResponse.<Void>builder()
+                            .code(1000)
+                            .message("Delete friendship successfully!")
+                            .build();
+    }
+
+    @PutMapping("/update/{id}/{status}")
+    public ApiResponse<FriendShipResponse> editFriendShipStatus(
+					@RequestParam Long id, 
+					@RequestParam FriendshipStatus status) {
+        	var response = this.friendShipService.editFriendShipStatus(id, status);
+            return ApiResponse.<FriendShipResponse>builder()
+                            .code(1000)
+                            .message("Edit friendship status successfully!")
+                            .result(mapper.toFriendShipResponse(response))
+                            .build();
+    }
+
+}
---
+++ user-service/src/main/java/ChitChat/user_service/controller/UserController.java
@@ -0,0 +1,130 @@
+package ChitChat.user_service.controller;
+
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import ChitChat.user_service.dto.request.UserCreationRequest;
+import ChitChat.user_service.dto.request.UserUpdateRequest;
+import ChitChat.user_service.dto.response.ApiResponse;
+import ChitChat.user_service.dto.response.UserDTO;
+import ChitChat.user_service.dto.response.UserResponse;
+import ChitChat.user_service.mapper.UserMapper;
+import ChitChat.user_service.service.UserService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+        private final UserService userService;
+        private final UserMapper userMapper;
+
+        // POST
+        @PostMapping("/create")
+        public ApiResponse<UserResponse> createUser(@Valid @RequestBody UserCreationRequest reqUser) {
+                var user = this.userService.createUser(reqUser);
+                return ApiResponse.<UserResponse>builder()
+                                .code(1000)
+                                .message("Create user successfully!")
+                                .result(userMapper.toUserResponse(user))
+                                .build();
+        }
+
+        // PUT
+        @PutMapping("/update")
+        public ApiResponse<UserResponse> updateUser(@RequestBody UserUpdateRequest reqUser) {
+                var user = this.userService.updateUser(reqUser);
+
+                return ApiResponse.<UserResponse>builder()
+                                .code(1000)
+                                .message("Update user with ID " + reqUser.getId() + " successfully")
+                                .result(userMapper.toUserResponse(user))
+                                .build();
+        }
+
+        // DELETE
+        @DeleteMapping("/delete/{id}")
+        public ApiResponse<Void> deleteUserById(@PathVariable Long id) {
+                this.userService.deleteUserById(id);
+                return ApiResponse.<Void>builder()
+                                .code(1000)
+                                .message("Delete user with ID " + id + " successfully!")
+                                .build();
+        }
+
+        // GET
+        // Get User by id
+        @GetMapping("/get/{id}")
+        public ApiResponse<UserResponse> getUserById(@PathVariable Long id) {
+                var dbUser = this.userService.getUser(id);
+                return ApiResponse.<UserResponse>builder()
+                                .code(1000)
+                                .message("Get user with ID " + id + " successfully!")
+                                .result(userMapper.toUserResponse(dbUser))
+                                .build();
+        }
+
+        // Get all Users
+        @GetMapping("/get/all")
+        public ApiResponse<Page<UserDTO>> getAllUsers(
+                        @RequestParam(defaultValue = "0") int page,
+                        @RequestParam(defaultValue = "20") int size) {
+                var users = this.userService.getAllUsers(page, size);
+                return ApiResponse.<Page<UserDTO>>builder()
+                                .code(1000)
+                                .message("Get all users successfully!")
+                                .result(users.map(userMapper::toUserDTO))
+                                .build();
+        }
+
+        // Get User's friends
+        @GetMapping("/get/friends")
+        public ApiResponse<Page<UserDTO>> getUserFriends(
+                        @RequestParam Long userId,
+                        @RequestParam(defaultValue = "0") int pageNum) {
+                var friends = this.userService.getUserFriends(userId, pageNum);
+                return ApiResponse.<Page<UserDTO>>builder()
+                                .code(1000)
+                                .message("Get friends of the user with ID " + userId + " successfully!")
+                                .result(friends)
+                                .build();
+        }
+
+        // Get mutual friends
+        @GetMapping("/get/mutualFriends")
+        public ApiResponse<Page<UserDTO>> getMutualFriends(
+                        @RequestParam Long meId,
+                        @RequestParam Long youId,
+                        @RequestParam(defaultValue = "0") int pageNum) {
+                var friends = this.userService.getMutualFriends(meId, youId, pageNum);
+                return ApiResponse.<Page<UserDTO>>builder()
+                                .code(1000)
+                                .message("Get mutual friends of user with ID " + meId +
+                                                " and user with ID " + youId + " successfully!")
+                                .result(friends)
+                                .build();
+        }
+
+        // Get User's friends
+        @GetMapping("/get/friends/suggested")
+        public ApiResponse<Page<UserDTO>> getSuggestedFriends(
+                        @RequestParam Long userId,
+                        @RequestParam(defaultValue = "0") int pageNum) {
+                var friends = this.userService.getSuggestedFriends(userId, pageNum);
+                return ApiResponse.<Page<UserDTO>>builder()
+                                .code(1000)
+                                .message("Get suggested friends of the user with ID " + userId + " successfully!")
+                                .result(friends)
+                                .build();
+        }
+}
