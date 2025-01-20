0200ae55ea4f38c19568d333b5b9f20a2047c0af
Luc Pham <phamtienluc0601@gmail.com>
2025-01-20 17:27:43 +0700

    update things

--- notification-service/src/test/java/ChitChat/notification_service/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/ChitChat/notification_service/NotificationServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package ChitChat.user_service;
+package ChitChat.notification_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserServiceApplicationTests {
+class NotificationServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
